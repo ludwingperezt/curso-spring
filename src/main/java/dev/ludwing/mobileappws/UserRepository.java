3047f87219b3c6c1fcdbca04ad520d5920213c9a
Ludwing <ludwing@localhost>
2022-11-23 19:44:33 -0600

    Agregar la interfaz de repository de usuarios.

---
+++ src/main/java/dev/ludwing/mobileappws/UserRepository.java
@@ -0,0 +1,29 @@
+package dev.ludwing.mobileappws;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import dev.ludwing.mobileappws.io.entity.UserEntity;
+
+/**
+ * Sin no se utiliza Spring Data JPA, entonces sería necesario crear
+ * una clase Data Access Object manualmente para realizar operaciones CRUD
+ * siendo necesario gestionar manualmente las conexiones a base de datos
+ * y el SQL a ejecutar.
+ * 
+ * La annotation @Repository indica a Spring que esta interfaz será un repositorio
+ * CRUD.
+ * 
+ * No es necesario definir los métodos CRUD, pero pueden agregarse métodos personalizados,
+ * por ejemplo para búsquedas sobre campos específicos, etc.
+ * 
+ * @author ludwingp
+ *
+ */
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+
+	// El método findUserByEmail() es un ejemplo de método personalizado para buscar un 
+	// usuario con base al email.
+	// UserEntity findUserByEmail(String email);
+}
