ee8ca30ba73ae0d814bbf0c86d8e8e3c3eff4c2e
Ludwing <ludwing@localhost>
2022-11-25 13:22:49 -0600

    Actualización de autenticación por WebSecurityConfigurerAdapter deprecada.
    Debido a que WebSecurityConfigurerAdapter fue deprecada, se actualizó la forma
    en que se autoriza el endpoint de sign-up para que esté disponible sin
    autorización.

--- src/main/java/dev/ludwing/mobileappws/security/WebSecurity.java
+++ src/main/java/dev/ludwing/mobileappws/security/WebSecurity.java
@@ -1,15 +1,19 @@
 package dev.ludwing.mobileappws.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import dev.ludwing.mobileappws.service.UserService;
 
+/**
+ * El código de esta sección está comentado porque WebSecurityConfigurerAdapter fue deprecado,
+ * y quedó aquí solo por razones demostrativas.  La implementación correcta es la que se encuentra
+ * por debajo de ésta sección comentada.
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 	
@@ -34,4 +38,43 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 		auth.userDetailsService(userDetailService).passwordEncoder(encoder);
 	}
 
+}
+*/
+
+@EnableWebSecurity
+public class WebSecurity {
+	
+	private final UserService userDetailService;  // Aqu{i se puede usar UserService porque esa clase heredó de UserDetailsService
+	private final BCryptPasswordEncoder encoder;
+	
+	public WebSecurity(UserService detailService, BCryptPasswordEncoder encoder) {
+		this.userDetailService = detailService;
+		this.encoder = encoder;
+	}
+	
+	@Bean
+	protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+		
+		// Aquí se configura el objeto AuthenticationManager.
+		// Como en la clase WebSecurity no hereda de WebSecurityConfigureAdapter se debe hacer de esta
+		// manera:
+		AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
+		
+		// Aquí se configura el builder del AuthenticationManager con el servicio que obtiene los
+		// datos del usuario de la base de datos y con el objeto que cifra las contraseñas.
+		builder.userDetailsService(userDetailService).passwordEncoder(encoder);
+		
+		// El AuthenticationManager es un objeto que se puede usar en las filter classes. 
+		// AuthenticationManager authManager = builder.build();
+		// http.authenticationManager(authManager);
+		
+		// CSRF se desactiva porque no se utiliza en stateless API's
+		// Luego se configura que todas las peticiones POST a /users no requieran autenticación
+		// Por último, todas las demás peticiones Sí deben ir autenticadas.
+		http.csrf().disable()
+			.authorizeHttpRequests().antMatchers(HttpMethod.POST, "/users").permitAll()
+			.anyRequest().authenticated();
+		
+		return http.build();
+	}
 }
