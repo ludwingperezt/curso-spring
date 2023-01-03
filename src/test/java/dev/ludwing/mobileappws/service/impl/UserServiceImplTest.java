e23b08ffe009bacd3f063ad3f3f48fada29d16e2
Ludwing <ludwing@localhost>
2023-01-03 13:06:07 -0600

    Agregado el primer test.

---
+++ src/test/java/dev/ludwing/mobileappws/service/impl/UserServiceImplTest.java
@@ -0,0 +1,63 @@
+package dev.ludwing.mobileappws.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import dev.ludwing.mobileappws.io.entity.UserEntity;
+import dev.ludwing.mobileappws.io.repositories.UserRepository;
+import dev.ludwing.mobileappws.shared.dto.UserDto;
+
+class UserServiceImplTest {
+	
+	// La clase UserServiceImpl NO se mockea porque es la clase que se está
+	// probando.
+	// La anotación @InjectMocks sirve para indicar que se deben inyectar los
+	// Mocks indicados al momento de instanciar el objeto.
+	@InjectMocks
+	UserServiceImpl userService;
+	
+	// Aquí estamos indicando que se hará mocking del objeto UserRepository
+	@Mock
+	UserRepository userRepository;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		MockitoAnnotations.openMocks(this);
+	}
+
+	@Test
+	void testGetUser() {
+		
+		// Aquí estamos creando un objeto dummy para que sea retornado por el Mock
+		// cuando se llame al método findUserByEmail
+		// Los datos del objeto por el momento no importan.
+		UserEntity userEntity = new UserEntity();
+		userEntity.setId(1l);
+		userEntity.setFirstName("Ludwing");
+		userEntity.setUserId("xxxxx");
+		userEntity.setEncryptedPassword("asdf1234");
+		
+		// Aquí se indica que cuando se utilice el método "findUserByEmail()" el cual
+		// puede recibir cualquier cadena de texto, entonces debe retornar una instancia
+		// de UserEntity, para la cual se usa el objeto dummy recién creado.
+		when(userRepository.findUserByEmail(anyString())).thenReturn(userEntity);
+		
+		// ///////////////////////////////////////////////////////////////////////////////
+		// Esta es la función a testear
+		UserDto userDto = userService.getUser("user@test.com");
+		// ///////////////////////////////////////////////////////////////////////////////
+		
+		// Sección de assertions donde se verifica que la función ha hecho lo que se supone que
+		// debe hacer:
+		assertNotNull(userDto);
+		assertEquals("Ludwing", userDto.getFirstName());
+	}
+
+}
